d664dc37935463ec279bea36091490affbb22cc2
Dreller <60242965+Dreller@users.noreply.github.com>
2021-02-09 08:08:37 -0500

    Suite menu
    Move suites in top navbar and split code to connect to DB.

--- php/sstm_navbar.php
+++ php/sstm_navbar.php
@@ -1,4 +1,16 @@
 <?php   
+session_start();
+include_once('sstm_db.inc');
+
+# Get Users's Account's Suites.
+$db->where('Account', $_SESSION['Account']);
+$db->orderBy('Name', 'asc');
+$suites = $db->get('suite');
+
+$suiteList = '';
+foreach($suites as $suite){
+    $suiteList .= '<a class="item" href="#" onclick="loadScreen(\'sstm_suite.php?id='.$suite['ID'].'\')">'.$suite['Name'].'</a>';
+}
 
 
 ?>
@@ -9,18 +21,20 @@
         Account Name
     </a>
     <a href="#" class="item">Home</a>
+    <div class="ui simple dropdown item">
+        Suites <i class="dropdown icon"></i>
+        <div class="menu">
+            <?php print $suiteList; ?>
+            <div class="divider"></div>
+            <a class="item" href="#" onclick="displayModal('NewSuite');"><i class="plus circle icon"></i>&nbsp;New suite</a>
+        </div>
+    </div>
         <div class="ui simple dropdown item">
         Setup <i class="dropdown icon"></i>
         <div class="menu">
             <a class="item" href="#" onclick="loadScreen('sstm_list.php?el=suite');">Suites</a>
-            <div class="divider"></div>
-            <div class="header">Quick access</div>
-                <a class="item" href="#" onclick="loadScreen('sstm_list.php?el=test');">Tests</a>
-                <a class="item" href="#" onclick="loadScreen('sstm_list.php?el=user');">Users</a>
-            <div class="divider"></div>
-            <div class="header">Other</div>
-                <a class="item" href="#">Parameters</a>
-            </div>
+            <a class="item" href="#" onclick="loadScreen('sstm_list.php?el=user');">Users</a>
+            <a class="item" href="#">Parameters</a>
         </div>
     </div>
 </div>
